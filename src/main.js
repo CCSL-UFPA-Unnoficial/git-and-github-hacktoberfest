20fbbefe2a87f7dd39ade876a5fcf468603d542b
lubien <lubien1996@gmail.com>
2018-10-08 12:37:40 -0300

    Parse contributors

--- src/main.js
+++ src/main.js
@@ -1,3 +1,51 @@
+import path from 'path'
+import fs from 'fs'
+
+const contributorsFolder = path.join(__dirname, '../contributors')
+
 export default function main () {
   console.log('todo')
+}
+
+export function getAllFileNames (folder) {
+  return fs.readdirSync(folder)
+}
+
+export function getAllContributors (folder = contributorsFolder) {
+  return getAllFileNames(folder)
+    .filter(name => name !== '.gitkeep')
+    .map(filename => path.join(folder, filename))
+    .map(fullPath => fs.readFileSync(fullPath, 'utf8'))
+}
+
+export function parseContributor (fullPath, rawContributor) {
+  if (typeof rawContributor !== 'string') {
+    throw new Error(`Invalid ${fullPath} type`)
+  }
+
+  const lines = rawContributor.split('\n')
+
+  if (lines.length !== 3) {
+    throw new Error(`Contributor ${fullPath} should have 3 lines. Has ${lines.length}`)
+  }
+
+  if (lines.some(line => !line.length)) {
+    throw new Error(`Contributor ${fullPath} should not have empty lines.`)
+  }
+
+  const [name, username, message] = lines
+
+  if (!username.startsWith('@')) {
+    throw new Error(`Contributor ${fullPath} username (second line) should start with @.`)
+  }
+
+  if (message.length > 140) {
+    throw new Error(`Contributor ${fullPath} message is too long (> 140 characters).`)
+  }
+
+  return {
+    name, 
+    username: username.slice(1),  // remove @
+    message
+  }
 }
--- test/main.test.js
+++ test/main.test.js
@@ -1,4 +1,106 @@
 // TODO
-test('1 === 1', () => {
-  expect(1).toBe(1)
+import path from 'path'
+
+import {
+  getAllFileNames,
+  getAllContributors,
+  parseContributor
+} from '..'
+
+const contributorsFolder = path.join(__dirname, './fixtures/contributors')
+
+test('getAllFileNames', () => {
+  const got = getAllFileNames(contributorsFolder)
+  const expected = [
+    '.gitkeep',
+    'a',
+    'b',
+    'c'
+  ]
+  expect(got).toEqual(expected)
+})
+
+test('getAllContributors', () => {
+  const got = getAllContributors(contributorsFolder)
+  expect(got.length).toBe(3)
+})
+
+test('parseContributor invalid', () => {
+  const fakePath = '/path/'
+  const contributor = {}
+
+  const got = () => parseContributor(fakePath, contributor)
+  expect(got).toThrowError(/invalid/i)
+})
+
+test('parseContributor no lines', () => {
+  const fakePath = '/path/'
+  const contributor = ''
+
+  const got = () => parseContributor(fakePath, contributor)
+  expect(got).toThrowError(/should have 3 lines/)
+})
+
+test('parseContributor wrong line count', () => {
+  const fakePath = '/path/'
+  const contributor = [
+    'Lubien',
+    '@lubien'
+  ].join('\n')
+
+  const got = () => parseContributor(fakePath, contributor)
+  expect(got).toThrowError(/should have 3 lines/)
+})
+
+test('parseContributor empty lines', () => {
+  const fakePath = '/path/'
+  const contributor = [
+    '',
+    '',
+    ''
+  ].join('\n')
+
+  const got = () => parseContributor(fakePath, contributor)
+  expect(got).toThrowError(/should not have empty lines/)
+})
+
+test('parseContributor invalid username', () => {
+  const fakePath = '/path/'
+  const contributor = [
+    'Lubien',
+    'lubien',
+    'My Message'
+  ].join('\n')
+
+  const got = () => parseContributor(fakePath, contributor)
+  expect(got).toThrowError(/username \(second line\) should start with @/)
+})
+
+test('parseContributor message too long', () => {
+  const fakePath = '/path/'
+  const contributor = [
+    'Lubien',
+    '@lubien',
+    Array.from(Array(141), () => 'a')
+  ].join('\n')
+
+  const got = () => parseContributor(fakePath, contributor)
+  expect(got).toThrowError(/message is too long/)
+})
+
+test('parseContributor valid', () => {
+  const fakePath = '/path/'
+  const contributor = [
+    'Lubien',
+    '@lubien',
+    'Hello World'
+  ].join('\n')
+
+  const got = parseContributor(fakePath, contributor)
+  const expected = {
+    name: 'Lubien',
+    username: 'lubien',
+    message: 'Hello World'
+  }
+  expect(got).toEqual(expected)
 })
